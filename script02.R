70c34dcaf8281e139e920d538c1385d58b9259b5
oseiasbeu <oseiasbeu@outlook.com>
2020-09-26 11:38:58 -0300

    Commit final

--- script02.R
+++ script02.R
@@ -12,7 +12,7 @@ library(sparklyr)
 library(titanic)
 library(pryr)
 library(dplyr)
-library()
+library(tidytext)
 
 
 # Fazendo a conexão com o spark 
@@ -92,5 +92,42 @@ print(word_count, n= 10)
 # Conversão para o tipo de dados do R (saindo do spark)
 # Tf–idf -- o peso, a importância de uma sentença em um livro está totalmetne 
 #ligada a quantidade de vezes que a mesma se repete no livro
+#https://pt.wikipedia.org/wiki/Tf%E2%80%93idf
 word_count <- as_tibble(word_count)
 print(word_count,n = 20)
+
+#Aplicando a função tf-idf -- relaciona a contagem de palavras,
+# com a frequências das palavras e as importâncias no documento
+
+word_tfidf <- word_count %>%
+  bind_tf_idf(word,author,n)
+
+#Encontrando o protagonista dos livro:
+# É só descobrir quem é o maior tf_idf dos livros
+word_tfidf %>%
+  arrange(desc(tf_idf))
+
+
+#COntando os primieros 20 tf-idf de cada autor
+twain20 <- word_tfidf %>%
+  arrange(desc(tf_idf)) %>%
+  filter(author == "twain")
+
+
+doyle20 <- word_tfidf %>%
+  arrange(desc(tf_idf)) %>%
+  filter(author == "doyle")
+
+print(twain20, n =20)
+print(doyle20, n =20)
+
+
+#Plotanto um gráfico com as palavras de maior mportância:
+#plot
+word_tfidf_filter <-  word_tfidf %>%
+  filter(tf_idf > 0.0002 & tf_idf < 0.0008)
+barplot(word_tfidf_filter$tf_idf,names.arg = word_tfidf_filter$word)
+
+
+#Tema para o exercício final:
+#Quais as palavras que usaria para difernecias Machado de Assis de Eça de Queirós?
